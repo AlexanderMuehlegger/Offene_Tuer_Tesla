fbfd31274e799d395ba8f16eba237ce5ac1c7241
Alexander Mühlegger <amuehlegger@tsn.at>
2022-10-15 14:44:15 +0200

    Initial Commit

---
+++ SWP_Tesla_Website/Models/DB/DB_DATA.cs
@@ -0,0 +1,12 @@
+﻿namespace SWP_Tesla_Website.Models.DB {
+    public class DB_DATA {
+        private static string IP = "localhost";
+        private static string port = "3306";
+        public static string database = "tesla";
+        private static string user = "root";
+        private static string password = "";
+
+        public static string connectionStr = (port == "")? $"server={IP};port={port};database={database};uid={user};password={password}"
+                                            : $"server={IP};database={database};uid={user};password={password}";
+    }
+}
---
+++ SWP_Tesla_Website/wwwroot/js/Notification.js
@@ -0,0 +1,53 @@
+﻿function newNotification(msg, status, reload){
+    const notiContainer = document.querySelector(".notification-container")
+    let notification = document.createElement("div")
+    notification.classList.add("notification")
+
+    let heading
+    
+    if (status.toLowerCase() == "failed" || status.toLowerCase() == "error" || status.toLowerCase() == "unauthorized") {
+        heading = status
+        notification.classList.add("failed")
+    }
+    else if (status.toLowerCase() == "success") {
+        heading = "Success"
+        notification.classList.add("success")
+    }
+    else if (status.toLowerCase() == "pending") {
+        heading = "Pending"
+         notification.classList.add("pending")    
+    }
+    else if (status.toLowerCase() == "coming_soon") {
+        heading = "Coming Soon"
+        notification.classList.add("coming_soon")
+    }
+    else
+        return "Error"
+    
+    let h1 = document.createElement("h1")
+    h1.innerHTML = heading
+    let h2 = document.createElement("h2")
+    h2.innerHTML = msg
+
+    let underline = document.createElement("div")
+    underline.classList.add("underline")
+
+    notification.appendChild(h1)
+    notification.appendChild(h2)
+    notification.appendChild(underline)
+    notiContainer.appendChild(notification)
+    
+    setTimeout(() => {
+        notification.classList.add("active")
+        
+    },250)
+
+    setTimeout(() => {
+        notification.classList.remove("active")
+        setTimeout(() => {
+            notiContainer.removeChild(notification)
+            if(reload)
+                location.reload()
+        }, 500)
+    }, 3500)
+}
